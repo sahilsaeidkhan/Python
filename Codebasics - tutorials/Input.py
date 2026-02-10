d39df1491fe8c7cc68f96290201d186c368fb780
Sahil Saeid <sahilsaeidkhan610@gmail.com>
2026-02-10 10:46:00 +0530

    change

--- Codebasics - tutorials/Input.py
+++ Codebasics - tutorials/Input.py
@@ -2,6 +2,13 @@
 #should print an area of a triangle
 
 
+#base = input("Enter base ")
+#height = input("Enter height ")
+
+ # area = 1/2 * int (base) * int(height) ;
+
+#print(area)
+
 
 
 
@@ -39,3 +46,15 @@
 
 #2. Write a program that takes file name with extension as an input and prints just the file name without extension (you
 #can assume that file extensions are always 3 character long)
+
+
+file_name = input("enter file with extension ")
+
+final = file_name[:-4]
+
+print(final)
+
+
+
+
+
