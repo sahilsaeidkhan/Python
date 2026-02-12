2474ca047c81cd4b70e1c1426af72d8e34cfe383
Sahil Saeid <sahilsaeidkhan610@gmail.com>
2026-02-12 10:57:53 +0530

    Update code as per latest changes

--- Codebasics - tutorials/dictionary.py
+++ Codebasics - tutorials/dictionary.py
@@ -28,3 +28,109 @@ for i in range(4):
      print(countries["query"])
     else:
       print("Country doesn't exist")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+prices = { "info" : [600,630,620]  , "ril" : [1430 , 1490 , 1397]  ,  "mtl" : [234,180,70] }
+
+operation = input("Enter any operation( print or add):").lower()
+
+if operation == "print":
+    for stock,price in prices.items():
+        avg = sum(price) / len(price)
+        print(f"{stock} ==> {price} ==> {avg:.2f}")
+
+
+elif operation == "add":
+    stock_name = input("Enter name of the stock-")
+    price = int(input("Enter price of the stock-"))
+
+    if stock_name in prices:
+        prices[stock_name].append(price)
+    else:
+        prices[stock_name] = [price]
+
+print(prices)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def circle_calc(r):
+
+  print(f"Diameter is {r*2}")
+  print(f"Circumference is {2*3.14*r*r}")
+  print(f"Area is {3.14*r*r}")
+
+r = int(input("Enter Radius:"))
+circle_calc(r)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from dictionary import circle_calc
+
+
+circle_calc(5)
+
+
+
+
