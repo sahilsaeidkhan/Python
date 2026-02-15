b1fc1861d29a1f4dfc2ba542942451651d164c92
Sahil Saeid <sahilsaeidkhan610@gmail.com>
2026-02-16 00:25:08 +0530

    m

--- Codebasics - tutorials/recursion.py
+++ Codebasics - tutorials/recursion.py
@@ -10,3 +10,27 @@ def sum ( n):
 print(sum (5))
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+# write a recursive function to print all elements in a list
+
+books = ["Do Epic Shit,","The Metamorphosis,","The Almanack Of Naval Ravikant"]
+
+def relist(index , books):
+    if(index==2):
+        return print(books[index])
+    print(books[index] ,  end = " ")
+    relist(index+1,books)
+
+print(relist(0,books))
